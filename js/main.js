4e640ee12f312904d9b61b3a357fd4b5bf6e0adb
Jamie Burridge <jjb98b@gmail.com>
2022-03-10 14:17:23 +0000

    Added interactive liquid pouring. Fixed: #12

---
+++ js/main.js
@@ -0,0 +1,39 @@
+/*--------
+Variables
+--------*/
+const machineButtons = document.querySelectorAll(".machine_button");
+const allNozzles = document.querySelectorAll(".machine_nozzle");
+
+/*---------------
+Pouring animation
+---------------*/
+function pourLiquid() {
+  for (let i = 0; i < 2; i++) {
+    machineButtons[i].addEventListener("click", () => {
+
+      // Pour milk or coffee depending on button clicked
+      if (machineButtons[i] == machineButtons[0]) {
+        allNozzles[0].classList.add("coffee_nozzle");
+      }
+      else if (machineButtons[i] == machineButtons[1]){
+        allNozzles[1].classList.add("milk_nozzle");
+      }
+
+      // Change pressed button color and change cursor
+      machineButtons[i].classList.add("machine_button_pressed");
+      machineButtons[i].style.cursor = "default";
+
+      // If both milk and coffee are poured then coffee is made
+      if (allNozzles[0].classList.contains("coffee_nozzle") && allNozzles[1].classList.contains("milk_nozzle")) {
+        setTimeout(() => {
+          console.log("Coffee is done");
+        }, 1000);
+      }
+    })
+  }
+}
+
+pourLiquid();
+
+
+
