a5fa559febe8e8ceca42f6e695b5370dfa03f851
Jamie Burridge <jjb98b@gmail.com>
2022-03-10 14:55:02 +0000

    Disabled coffee being made twice. Fixed: #15

--- js/main.js
+++ js/main.js
@@ -10,24 +10,27 @@ Pouring animation
 function pourLiquid() {
   for (let i = 0; i < 2; i++) {
     machineButtons[i].addEventListener("click", () => {
-
       // Pour milk or coffee depending on button clicked
       if (machineButtons[i] == machineButtons[0]) {
-        allNozzles[0].classList.add("coffee_nozzle");
+          allNozzles[0].classList.add("coffee_nozzle");
       }
       else if (machineButtons[i] == machineButtons[1]){
-        allNozzles[1].classList.add("milk_nozzle");
+          allNozzles[1].classList.add("milk_nozzle");  
       }
 
-      // Change pressed button color and change cursor
+      // Change pressed button color
       machineButtons[i].classList.add("machine_button_pressed");
+      // Change pressed button cursor
       machineButtons[i].style.cursor = "default";
+      // Diable button from being clicked
+      machineButtons[i].style.pointerEvents = "none";  
 
       // If both milk and coffee are poured then coffee is made
-      if (allNozzles[0].classList.contains("coffee_nozzle") && allNozzles[1].classList.contains("milk_nozzle")) {
+      while (allNozzles[0].classList.contains("coffee_nozzle") && allNozzles[1].classList.contains("milk_nozzle")) {
         setTimeout(() => {
           console.log("Coffee is done");
         }, 1000);
+        break;
       }
     })
   }
